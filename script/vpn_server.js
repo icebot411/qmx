5464e2cf18be091463d3a0750b0ce9395f6fa7f0
icebot411 <33836845+icebot411@users.noreply.github.com>
2024-08-10 09:29:44 -0700

    Update vpn_server.js

--- script/vpn_server.js
+++ script/vpn_server.js
@@ -4,10 +4,11 @@ var obj = JSON.parse(server_data);
 var serverinfo = {
   "premium": 0,
 }
-for (let key in obj.data.*) {
+for (let key in obj.data) {
   if (serverinfo.hasOwnProperty(key)) {
     obj.data[key] = serverinfo[key]
   }
 }
 server_data = JSON.stringify(obj);
-$done
+$done(server_data);
+
