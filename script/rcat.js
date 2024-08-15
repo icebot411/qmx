ffe8abda37682e9b501e64944237e8419e656ba4
icebot411 <33836845+icebot411@users.noreply.github.com>
2024-08-15 06:07:40 -0700

    +

--- script/rcat.js
+++ script/rcat.js
@@ -42,11 +42,7 @@ const data2 = {
     "store":"app_store",
     "unsubscribe_detected_at":null
       };
-      const data3 = {
 
-        "original_purchase_date":"2022-04-09T05:05:04Z"
-          };
-    
 if (typeof $response == "undefined") {
 	delete $request.headers["x-revenuecat-etag"]; // prevent 304 issues
 	delete $request.headers["X-RevenueCat-ETag"];
@@ -57,7 +53,6 @@ if (typeof $response == "undefined") {
 	for (const i in list) {
 		if (new RegExp(`^${i}`, `i`).test(ua)) {
 			obj.subscriber.subscriptions[list[i].id] = data2;
-            obj.subscriber[list[i]] = data3;
             obj.subscriber.entitlements[list[i].name] = JSON.parse(JSON.stringify(data));
             obj.subscriber.entitlements[list[i].name].product_identifier = list[i].id;
 			break;
