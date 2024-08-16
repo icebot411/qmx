f60af24b72a75f60deb244e4c858403082a5cac8
icebot411 <33836845+icebot411@users.noreply.github.com>
2024-08-16 10:56:45 -0700

    Update Revenuecat.js

--- script/Revenuecat.js
+++ script/Revenuecat.js
@@ -64,6 +64,7 @@ if (typeof $response == "undefined") {
   const app = '1';
   const UAMappings = {
     'iScape':{ name: 'Pro', id: 'Limited_YearlyProAutoRenew'},//8.15
+    'CCM':{ name: 'Subscriptions', id: 'ccml_ios_monthly_v1'},//8.15
     'FunPix':{ name: 'premium', id: 'intro_price_weekly'},//8.15
     'WiseMate':{ name: 'vip_entitlement', id: 'wisemate.ai.ios.week'},//8.15
     'Loora':{ name: 'Yearly', id: 'yearly_119_99_no_trial'},//8.14
