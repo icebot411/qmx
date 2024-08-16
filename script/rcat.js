0a4d7e439cb283fcfd1ed148731f4d75c77b229d
icebot411 <33836845+icebot411@users.noreply.github.com>
2024-08-16 11:53:57 -0700

    +

--- script/rcat.js
+++ script/rcat.js
@@ -25,7 +25,7 @@ const list = {
 	'HTTPBot': { name: 'rc_lifetime', id: 'com.behindtechlines.HTTPBot.prounlock' },
 	'VSCO': { name: 'membership', id: 'com.circles.fin.premium.yearly' },
 	'1Blocker': { name: 'premium', id: 'blocker.ios.subscription.yearly' },
-    'Gear': { name: 'rc_annual', id: 'com.gear.app.yearly'}
+    'CCML': { name: 'rc_monthly', id: 'ccml_ios_monthly_v1' }
 };
 const data = {
 	"expires_date": "2030-02-18T07:52:54Z",
