73a51a0ef3761321e0358253fea72f75eea3d0c4
icebot411 <33836845+icebot411@users.noreply.github.com>
2024-09-03 11:18:36 -0700

    Update termi.js

--- script/termi.js
+++ script/termi.js
@@ -1,20 +1,26 @@
 var obj = JSON.parse($response.body);
-obj.result["pro_mode"] = true;
-obj.result["until"] = "2099-09-17T14:41:12";
-obj.result["valid_until"] = "2099-09-17T14:41:12";
-obj.result["is_active"] = true;
-obj.result["personal_subscription"] = true;
-obj.result["need_to_update_subscription"] = false;
-
-obj.result["access_objects"] = [
-    {
-        "period": {
-          "start": "2024-09-03T14:41:12",
-          "end": "2099-09-17T14:41:12"
-        },
-        "title": "Pro"
-      }
-  ];
-
-
 $done({body : JSON.stringify(obj)});
+obj=
+    {
+        "pro_mode":true, 
+        "until":"2099-09-17T14:41:12",
+        "valid_until":"2099-09-17T14:41:12",
+        "is_active":true,
+        "personal_subscription":true,
+        "need_to_update_subscription":false,
+
+        "access_objects":[
+            {
+                "period": {
+                  "start": "2024-09-03T14:41:12",
+                  "end": "2099-09-17T14:41:12"
+                },
+                "title": "Pro"
+              }
+          ],
+    }
+    
+    
+$done({
+    body: JSON.stringify(obj)
+  });
